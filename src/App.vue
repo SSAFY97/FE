51c91493357d3490ddee80a0d8b24e5cfb7c028f
sion <sion691@gmail.com>
2026-07-14 17:22:31 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Vue 3 + Vite 개발 환경</h1>
+    <p>이제 개발을 시작할 수 있습니다.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 4rem;
+}
+</style>
